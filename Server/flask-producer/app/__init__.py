12fcfdc88e296685b771f21352c5e7bb83415a33
Milla <kziatdinova@gmail.com>
2022-06-13 02:26:39 +0300

    Initial commit

---
+++ Server/flask-producer/app/__init__.py
@@ -0,0 +1,14 @@
+# from .config import Config
+# from flask_mongoengine import MongoEngine
+from celery import Celery
+from flask import Flask
+
+app = Flask(__name__)
+cel_app = Celery('tasks',
+                 broker='amqp://user:pass@rabbit:5672',
+                 backend='mongodb://mongodb:27017/backdb')
+# app.config.from_object(Config)
+
+# db = MongoEngine()
+# db.init_app(app)
+
