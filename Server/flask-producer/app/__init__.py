530f5a20a1a08987468a88d84401ce6178cab38a
_milla_ <71004590+KamillaZiatdinova@users.noreply.github.com>
2022-06-13 02:28:37 +0300

    Update __init__.py

--- Server/flask-producer/app/__init__.py
+++ Server/flask-producer/app/__init__.py
@@ -7,8 +7,5 @@ app = Flask(__name__)
 cel_app = Celery('tasks',
                  broker='amqp://user:pass@rabbit:5672',
                  backend='mongodb://mongodb:27017/backdb')
-# app.config.from_object(Config)
 
-# db = MongoEngine()
-# db.init_app(app)
 
